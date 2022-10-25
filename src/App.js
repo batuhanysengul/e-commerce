a0a65627e38ee15cb97cf15d2d8d38013c110aee
Batuhan <batuhanyigitsengul@gmail.com>
2022-10-25 23:11:32 +0300

    will create components later

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
+import canimg from './assets/cans.png'
+import gsap, {Power2} from 'gsap';
+import {FaShoppingBag} from 'react-icons/fa';
 
 function App() {
+  const canRef = useRef(null);
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    gsap.from(canRef.current, {
+      duration: 1,
+      opacity: 0,
+      y: -500,
+    });
+
+    gsap.from(titleRef.current, {
+      duration: 1,
+      opacity: 0,
+      x: -800,
+      ease: Power2.easeOut,
+    })
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="navcontainer">
+        <div className="nav">
+        <h1 className='logo'>fresh⁰⁰</h1>
+        <FaShoppingBag className='bag' />
+        </div>
+      </div>
+      <div className='title'>
+        <div ref={titleRef}>
+        <h1>Deneme</h1>
+        <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+        <img src={canimg} alt="" ref={canRef} />
+      </div>
     </div>
   );
 }
